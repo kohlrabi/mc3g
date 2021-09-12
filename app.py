92cb2c06875bf56330b0bc29e90ffc9005ed3cac
Jan Christoph Terasa <christoph@kohlio.de>
2021-09-12 18:18:45 +0200

    Fix default value.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("2G/3G Monte Carlo")
 with st.sidebar.container():
     N = st.sidebar.slider("Anzahl Personen im Innenraum:", 0, 500, 200, 10)
     prevalence = np.logspace(-1, 1, 91, endpoint=True)
-    p = st.sidebar.select_slider("Prävalenz (%) (Standard: 0.1%):", prevalence, 1, format_func=lambda x: f'{x:.2f}%')
+    p = st.sidebar.select_slider("Prävalenz (%) (Standard: 1.0%):", prevalence, 1, format_func=lambda x: f'{x:.2f}%')
     ve = st.sidebar.slider("Impfstoffwirksamkeit (%) (Standard 66%):", 0, 100, 66, 1, format='%d%%')
     vq = st.sidebar.slider("Impfquote (%) (Standard: 80%):", 0, 100, 80, 1, format='%d%%')
     sens = st.sidebar.slider("Test-Sensitivität (%) (Standard: 80%, Minimum: 1%):", 1, 100, 80, 1, format='%d%%')
