1bd14c7b94c1dde9a2de07d2af42d9c8ed5625e1
Jan Christoph Terasa <christoph@kohlio.de>
2021-09-12 23:34:27 +0200

    Slightly different formatting for number input fields.

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ st.set_page_config(page_title="2G/3G Monte Carlo", initial_sidebar_state="expand
 st.title("2G/3G Monte Carlo")
 
 with st.sidebar.container():
-    N = st.sidebar.number_input("Anzahl Personen im Innenraum:", 0, 10_000, 100, 1, format='%d')
-    p = st.sidebar.number_input("Prävalenz (%) (Standard: 1.0%):", 1e-3, 100., 1., 1e-4, format='%.2f')
-    ve = st.sidebar.number_input("Impfstoffwirksamkeit (%) (Standard 66%):", -100, 100, 66, 1, format='%d')
-    vr = st.sidebar.number_input("Impfquote (%) (Standard: 80%):", 0, 100, 80, 1, format='%d')
-    sens = st.sidebar.number_input("Test-Sensitivität (%) (Standard: 80%, Minimum: 1%):", 1, 100, 80, 1, format='%d')
-    spec = st.sidebar.number_input("Test-Spezifität (%) (Standard: 97%, Minimum: 1%):", 1, 100, 97, 1, format='%d')
+    N = st.sidebar.number_input("Anzahl Personen im Innenraum (Minimum: 0, Maximum: 10.000, Standard: 100):", 0, 10_000, 100, 1, format='%d')
+    p = st.sidebar.number_input("Prävalenz (%) (Minimum: 0.01%, Maximum: 100%, Standard: 1.0%):", 1e-2, 100., 1., 1e-3, format='%.2f')
+    ve = st.sidebar.number_input("Impfstoffwirksamkeit (%) (Minimum: -100%, Maximum: 100%, Standard 66%):", -100., 100., 66., 1e-2, format='%.2f')
+    vr = st.sidebar.number_input("Impfquote (%) (Minimum: 0%, Maximum: 100%, Standard: 80%):", 0., 100., 80., 1e-2, format='%.2f')
+    sens = st.sidebar.number_input("Test-Sensitivität (%) (Minimum: 1%, Maximum: 100%, Standard: 80%):", 1., 100., 80., 1e-2, format='%.2f')
+    spec = st.sidebar.number_input("Test-Spezifität (%) (Minimum: 1%, Maximum: 100%, Standard: 97%):", 1., 100., 97., 1e-2, format='%.2f')
 
 fig = plot(vr/100, ve/100, p/100., N, sens/100, spec/100)
 st.pyplot(fig)
